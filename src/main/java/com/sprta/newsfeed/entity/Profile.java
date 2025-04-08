17731ec8d0256ac5338ad25de94ca7021908ef09
haneul02 <redlight02@naver.com>
2025-04-08 11:27:22 +0900

    feat: 프로필 엔티티 생성

---
+++ src/main/java/com/sprta/newsfeed/entity/Profile.java
@@ -0,0 +1,26 @@
+package com.sprta.newsfeed.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.repository.config.AuditingBeanDefinitionParser;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "profile")
+public class Profile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+     @Column(name = "id")
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private String introduction;
+}
+
+
