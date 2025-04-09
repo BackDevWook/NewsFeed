29ad113b6eebcdd374e3e18320e58b08ed70f79b
BackDevWook <https://github.com/BackDevWook>
2025-04-09 17:14:00 +0900

    내용 없음

--- src/main/java/com/sprta/newsfeed/dto/FollowCountResponseDto.java
+++ src/main/java/com/sprta/newsfeed/dto/FollowCountResponseDto.java
@@ -2,9 +2,11 @@ package com.sprta.newsfeed.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor(force = true)
 public class FollowCountResponseDto {
 
     // 팔로잉, 팔로워 수
