5bef84c940bce243e00a2ddb5fb05501766bf924
xaehub <novxae@gmail.com>
2025-04-11 11:13:24 +0900

    feat: 게시물의 댓글 수 자동 증가/감소 기능 추가 - Post entity에 댓글 수 증가/감소 생성자 추가 - CommentService 댓글 생성, 삭제할 때 댓글 수 증가/감소 기능 추가

--- src/main/java/com/sprta/newsfeed/entity/Post.java
+++ src/main/java/com/sprta/newsfeed/entity/Post.java
@@ -59,5 +59,13 @@ public class Post extends BaseEntity {
     }
 
 
+    public void increaseCommentCount() {
+        this.countComments += 1;
+    }
+
+    public void decreaseCommentCount() {
+        this.countComments = Math.max(0, this.countComments - 1);
+    }
+
 
 }
