e28c91fe639503a5112065db0f3ec3651e67e143
KSH-a <kimjune.net@gmail.com>
2025-04-11 12:02:25 +0900

    fix : "code Refactory, Import 제거"

--- src/main/java/com/sprta/newsfeed/service/UserServiceImpl.java
+++ src/main/java/com/sprta/newsfeed/service/UserServiceImpl.java
@@ -14,8 +14,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.Optional;
-
 @Service
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository; // 사용자 저장소 (DB 연동)
